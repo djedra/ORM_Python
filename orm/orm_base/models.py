10f56216b6d2592cb8f549e499b0beed5cc9a510
Anton Ryabenko <djedra@yandex.ru>
2024-03-11 11:34:05 +0700

    init

---
+++ orm/orm_base/models.py
@@ -0,0 +1,69 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Publisher(Base):
+    __tablename__ = 'publisher'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=70), unique=True, nullable=False)
+
+    def __str__(self):
+        return f'Издатель: {self.id} - {self.name}'
+
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=100), unique=False, nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+
+    publisher = relationship(Publisher, backref='books')
+
+    def __str__(self):
+        return f'Книга: {self.id} - {self.title} - {self.id_publisher}'
+
+
+class Shop(Base):
+    __tablename__ = 'shop'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=100), unique=True, nullable=False)
+
+    def __str__(self):
+        return f'Магазин: {self.id} - {self.name}'
+
+
+class Stock(Base):
+    __tablename__ = 'stock'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+    count = sq.Column(sq.Integer, unique=False, nullable=False)
+
+    book = relationship(Book, backref='stocks')
+    shop = relationship(Shop, backref='stocks')
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Numeric, unique=False, nullable=False)
+    date_sale = sq.Column(sq.DateTime, unique=False, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
+    count = sq.Column(sq.Integer, unique=False, nullable=False)
+
+    stock = relationship(Stock, backref='sales')
+
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+
+def drop_tables(engine):
+    Base.metadata.drop_all(engine)
