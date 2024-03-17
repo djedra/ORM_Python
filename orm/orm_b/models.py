d8451f9e437e524bce15811dfdf981535bfae950
Anton Ryabenko <djedra@yandex.ru>
2024-03-17 11:04:26 +0700

    init

---
+++ orm/orm_b/models.py
@@ -0,0 +1,73 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+    # def __str__(self):
+    #     return f'Publisher {self.id}: {self.name}'
+
+
+class Shop(Base):
+    __tablename__ = "shop"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+    def __str__(self):
+        return f'Shop {self.id}: {self.name}'
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.Text, nullable=False)
+    publisher_id = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+
+    publisher = relationship(Publisher, backref="book")
+
+    def __str__(self):
+        return f'Book {self.id}: ({self.title}, {self.publisher_id})'
+
+
+class Stock(Base):
+    __tablename__ = "stock"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    book_id = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    shop_id = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+    book = relationship(Book, backref="stock")
+    shop = relationship(Shop, backref="stock")
+
+    def __str__(self):
+        return f'Stock {self.id}: ({self.book_id}, {self.shop_id}, {self.count})'
+
+
+class Sale(Base):
+    __tablename__ = "sale"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.DateTime, nullable=False)
+    stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+    stock = relationship(Stock, backref="sale")
+
+    def __str__(self):
+        return f'Sale {self.id}: ({self.price}, {self.date_sale}, {self.stock_id}, {self.count})'
+
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
